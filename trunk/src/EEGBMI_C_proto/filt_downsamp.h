980353156cc3eae709ea28b3713924d454c90f5e
atom2626 <atom2626@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2014-12-29 20:45:07 +0000

    EEGBMI data processing prototype.
    - wrapper for fake data, works fine
    - filtered-downsampler, implemented tested need to be called in the wrapper
    - FFT, all to be done (ref code exist and found)
    - SVM, source code (c++) is compilable, need to be tested and integrated with the rest.

---
+++ trunk/src/EEGBMI_C_proto/filt_downsamp.h
@@ -0,0 +1,12 @@
+#ifndef FILT_DOWNSAMP_H
+#define FILT_DOWNSAMP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void filt_downsamp_signal(float *input_signal, int nb_samples, float *output_signal);
+
+
+#endif
