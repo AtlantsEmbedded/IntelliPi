50d9107f9e6f86ad0827301a8da857669a11280d
atom2626 <atom2626@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-04-12 18:03:12 +0000

    Put some work on the linear algebra library.
    I changed the way the matrix methods work. Now the output matrix needs to be provided by reference as input parameter. The test bench hasn't been updated yet.
    
    I added several vector-vector and vector-scalar operations (add, sub, multiply, dot product). So far I only implemented the functions I need, but it shouldn't take long to populate the methods with all basic operations.
    
    I added my implementation of Lanczos algorithm, the first stage of the eigen solver of the fisher discriminant analysis.
    
    So far, nothing is tested
    
    TODO: build a test bench that tests all methods.

--- src/lin_algebra_lib/linear_algebra.h
+++ src/lin_algebra_lib/linear_algebra.h
@@ -5,14 +5,47 @@
 #include <stdlib.h>
 #include <math.h>
 
+/***************************/
+/* Basic Matrix operations */
+/***************************/
+/*A(i,j)*B(j,k) = C(i,k)*/
+void mtx_mult(double *A, double *B, double *C, int dim_i, int dim_j, int dim_k);
 
+/*A = A'*/
+void mtx_transpose(double *A, double *A_prime, int dim_i, int dim_j);
+
+/*A = LL'*/
 double *mtx_chol(double *A, int n);
 
-double *mtx_mult(double *A, double *B, int dim_i, int dim_j, int dim_k);
-
-double *mtx_transpose(double *A, int dim_i, int dim_j);
-
 void show_matrix(double *A, int n);
+
+/***************************/
+/* Basic Vector operations */
+/***************************/
+/*a = a+b*/
+void vect_add(double* a, double b*, int n);
+
+/*a = a-b*/
+void vect_sub(double* a, double b*, int n)
+
+/*a = a*b, where b is a scalar*/
+void vect_scalar_multiply(double* a, double b, int n);
+
+/*|a|*/
+double vect_norm(double* a, int n)
+
+/*a.b = sum(a[i]*b[i]) over all i*/
+double vect_dot_product(double* a, double b*, int n)
+
+/*V => random and |V|==1*/
+double* vect_rand_unit(int length)
+
+/***************************/
+/* Eigen solver            */
+/***************************/
+/*A => Tm*/
+void mtx_lanczos_procedure(double *A, double *Tm, int n, int m);
+
 
 #endif
 
