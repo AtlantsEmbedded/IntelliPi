0e54df47439b78b8faff43b248333b8c3dcb35b1
lantzvillian <lantzvillian@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-06-07 05:38:42 +0000

    MUSE garbage

--- src/DATA_interface/data-daemon/src/supported_hardware/muse.c
+++ src/DATA_interface/data-daemon/src/supported_hardware/muse.c
@@ -44,7 +44,6 @@ int muse_cleanup(void *param __attribute__ ((unused)))
 	return (0);
 }
 
-
 /**
  * muse_init_hardware()
  * @brief Initializes muse hardware related variables
@@ -62,7 +61,7 @@ int muse_init_hardware(void *param __attribute__ ((unused)))
  */
 int muse_translate_pkt(void *param)
 {
-	param_t *param_ptr __attribute__ ((unused)) = (param_t *) param ;
+	param_t *param_pt __attribute__ ((unused)) = (param_t *) param;
 
 	// Do some things to print out the packet
 	return (0);
@@ -82,6 +81,7 @@ int muse_send_keep_alive_pkt(void *param __attribute__ ((unused)))
 		status = send(get_socket_fd(), msg, 3, 0);
 		sleep(seconds);
 	} while (status > 0);
+
 	return (0);
 }
 
@@ -109,18 +109,56 @@ int muse_send_pkt(void *param)
  * @brief Processes the packet
  * @param param
  */
-int muse_process_pkt(void * param)
+int muse_process_pkt(void *param)
 {
 	// Uncompressed or raw at this point
 	param_t *param_ptr = (param_t *) param;
+	muse_raw_pkt_t *muse_raw_pkt_ptr = NULL;
+
+	uint8_t shim = 1;	// Header - type + flag == one byte
 	fprintf(stdout, "Bytes read = %d\n", param_ptr->len);
 	hexdump((unsigned char *)param_ptr->ptr, param_ptr->len);
+	if (param_ptr->len >= 6) {
+		muse_raw_pkt_ptr = (muse_raw_pkt_t *) param_ptr->ptr;
+		if ((muse_raw_pkt_ptr->flag >> 4) == 0x8) {
+			shim += 2;
+		}
+		printf("type: %01x\n", muse_raw_pkt_ptr->type & 0x0F);
+		switch ( muse_raw_pkt_ptr->type & 0x0F) {
+		case MUSE_UNCOMPRESS_PKT:
+			//~ muse_raw_pkt_ptr->ch1 = muse_raw_pkt_ptr + shim;
+			//~ muse_raw_pkt_ptr->ch2 = muse_raw_pkt_ptr + shim + 1;
+			//~ muse_raw_pkt_ptr->ch3 = muse_raw_pkt_ptr + shim + 2;
+			//~ muse_raw_pkt_ptr->ch4 = muse_raw_pkt_ptr + shim + 3;
+			//~ printf("ch1: %x\n", muse_raw_pkt_ptr->ch1);
+			//~ printf("ch2: %x\n", muse_raw_pkt_ptr->ch2);
+			//~ printf("ch3: %x\n", muse_raw_pkt_ptr->ch3);
+			//~ printf("ch4: %x\n", muse_raw_pkt_ptr->ch4);
 
-	if (_TRANS_PKT_FC)
-		TRANS_PKT_FC(param);
-	
+			printf("got a eeg pkt\n");
+			break;
+		case MUSE_COMPRESSED_PKT:
+			printf("got a compressed eeg pkt\n");
+			break;
+		default:
+			
+			return (0);
+			break;
+		}
+
+	} else {
+		printf("Invalid packet - too small\n");
+	}
+
+	//~ param_ob.ptr = muse_raw_pkt_ptr;
+
+
+
+	//~ if (_TRANS_PKT_FC) {
+	//~ TRANS_PKT_FC(&param_ob);
+	//~ }
+
 	return (0);
-
 }
 
 /**
@@ -132,12 +170,19 @@ int muse_read_pkt(void *param __attribute__ ((unused)))
 	int bytes_read = 0;
 	char buf[BUFSIZE] = { 0 };
 	param_t param_start_transmission = { MUSE_START_TRANSMISSION, 3 };
+	param_t param_request_transmission = { MUSE_VERSION, 5};
+	param_t param_preset_transmission = { MUSE_PRESET, 6};
+	param_t param_host_transmission = { MUSE_SET_HOST_PLATFORM, 5};
 	param_t param_translate_pkt = { 0 };
 
+	muse_send_pkt((void *)&param_request_transmission);
+	muse_send_pkt((void *)&param_host_transmission);
+	muse_send_pkt((void *)&param_preset_transmission);
+	
 	muse_send_pkt((void *)&param_start_transmission);
 
 	do {
-		
+
 		bytes_read = recv(get_socket_fd(), buf, BUFSIZE, 0);
 
 		if (bytes_read <= 0) {
