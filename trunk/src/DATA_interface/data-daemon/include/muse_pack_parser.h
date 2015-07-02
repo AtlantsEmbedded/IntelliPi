bb48f7473f90789908e20880c5b9038eb875a885
atom2626 <atom2626@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-07-02 19:40:46 +0000

    Finished the packet pre-parser that finds the soft packets in the bluetooth packet.

--- trunk/src/DATA_interface/data-daemon/include/muse_pack_parser.h
+++ trunk/src/DATA_interface/data-daemon/include/muse_pack_parser.h
@@ -13,7 +13,7 @@
  * https://sites.google.com/a/interaxon.ca/muse-developer-site/muse-communication-protocol/compressed-eeg-packets
  * 
  * And the general description of the Muse bluetooth protocol:
- * 
+ * https://sites.google.com/a/interaxon.ca/muse-developer-site/muse-communication-protocol/data-streaming-protocol
  */
 
 #include <stdio.h>
@@ -21,12 +21,29 @@
 #include <string.h>
 #include <math.h>
 
+#define MUSE_SYNC_PKT 0xF	 //First nibble of sync packet
+#define MUSE_UNCOMPRESS_PKT 0xE	 //Uncompressed EEG
+#define MUSE_ERR_PKT 0xD	 //Error Flags
+#define MUSE_COMPRESSED_PKT 0xC	 //Compressed EEG
+#define MUSE_BATT_PKT 0xB	 //Battery
+#define MUSE_ACC_PKT 0xA	 //Accelerometer
+#define MUSE_DRLREF_PKT  0x9	 //DRL/REF
+#define MUSE_INVALID  0x0	 //Invalid
 
-void compressed_parse_medians(char* medians_header, int* quantizations, int* medians);
+int get_packet_type(unsigned char packet_header);
 
-int compressed_parse_bit_length(char* bit_length_header);
+int preparse_packet(char* raw_packet_header, int packet_length, int *soft_packet_headers, int *soft_packet_types);
 
-int compressed_parse_deltas(char* bits_header, int* median, int* quantization, int* deltas);
+int parse_compressed_packet(unsigned char* packet_header, char* deltas);
+void compressed_parse_medians(unsigned char* medians_header, int* quantizations, int* medians);
+int compressed_parse_bit_length(unsigned char* bit_length_header);
+int compressed_parse_deltas(unsigned char* bits_header, int* median, int* quantization, int* deltas);
+
+void uncompressed_parse_values(unsigned char* values_header, int* values);
+
+inline int get_flag_value(unsigned char first_byte);
+
+
 
 
 
