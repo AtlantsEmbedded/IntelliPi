2c1816ecd71ef5588642a57f1e5271a3a4b2de64
atom2626 <atom2626@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-10-03 21:43:24 +0000

    new LED test

--- trunk/src/WearHacks2015/cerebralwars_app/main.c
+++ trunk/src/WearHacks2015/cerebralwars_app/main.c
@@ -21,11 +21,21 @@ int main(int argc, char **argv){
 	int i;
 	int spi_driver;
 	
-	unsigned char* buffer[6] = {0xff,0xaa,0xff,0xaa,0xff,0xaa};
+	unsigned char* buffer[3*150];
+	
+	memset(buffer,0,3*150);
+	
+	for(i=0;i<150;i+=2){
+		buffer[i*3+0] = 0xFF;
+	}
+	
+	for(i=1;i<150;i+=2){
+		buffer[i*3+1] = 0x7F;
+	}
 	
 	/*configure the mind box*/
 	spi_driver = open("/dev/spidev0.0",O_RDWR);
-	write(spi_driver, buffer, 6);
+	write(spi_driver, buffer, 3*150);
 	
 	close(spi_driver);
 	
