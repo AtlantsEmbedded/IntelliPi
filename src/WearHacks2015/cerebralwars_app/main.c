f536ddc018d0ab689cb6165bc7034250626e856a
atom2626 <atom2626@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-10-03 23:00:58 +0000

    Added a struct for uint16 adressing

--- src/WearHacks2015/cerebralwars_app/main.c
+++ src/WearHacks2015/cerebralwars_app/main.c
@@ -11,6 +11,13 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 
+typedef struct pixel_s{
+	uint16_t first_bit:1;
+	uint16_t red:5;
+	uint16_t green:5;
+	uint16_t blue:5;
+}pixel_t;
+
 
 /**
  * main(int argc, char **argv)
@@ -21,21 +28,23 @@ int main(int argc, char **argv){
 	int i;
 	int spi_driver;
 	
-	unsigned char* buffer[3*150];
+	/*define buffer*/
+	pixel_t* buffer[150];
 	
-	memset(buffer,0,3*150);
+	/*set the whole array to 0*/
+	memset(buffer,0,150*sizeof(pixel_t));
 	
 	for(i=0;i<150;i+=2){
-		buffer[i*3+0] = 0xFF;
+		buffer[i].red = 0x1F;
 	}
 	
 	for(i=1;i<150;i+=2){
-		buffer[i*3+1] = 0x7F;
+		buffer[i].green = 0x1F;
 	}
 	
 	/*configure the mind box*/
 	spi_driver = open("/dev/spidev0.0",O_RDWR);
-	write(spi_driver, buffer, 3*150);
+	write(spi_driver, buffer, 150*sizeof(pixel_t));
 	
 	close(spi_driver);
 	
