4d3c9f0ad33ffd153fadd90e55a27a5c4740464c
billy4568 <billy4568@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-10-04 14:52:03 +0000

    Test for the cerebral app testbench with the leds and start button

--- src/mindbx_testbench/src/main.c
+++ src/mindbx_testbench/src/main.c
@@ -17,30 +17,65 @@ int main(int argc, char **argv){
 	
 	/*configure the mind box*/
 	setup_mindbx();
-	
+	 
+	/*Test the outputs of the LEDs*/
+	 
+	 /*Player 1 LEDs*/
+	 set_player1_green();
+	 delay(1000);
+	 reset_player1_green();
+	 set_player1_red();
+	  delay(1000);
+	 reset_player1_red();
+	 
+	 
+	 /*Player 2 LEDs*/
+	 set_player2_green();
+	  delay(1000);
+	 reset_player2_green();
+	 set_player2_red();
+	  delay(1000);
+	 reset_player2_red();
+	 
+	 
+	 /*Test the flashing leds for player 1*/
+	 set_player1_flash_green();
+	 set_player1_flash_red();
+	 
+	 /*Test the flashing leds for player 2*/
+	 set_player2_flash_green();
+	 set_player2_flash_red();
+	 
+	 /*Test the start button*/
+	 
+	 wait_for_test_button();
+	 
+	 
+	 
+	 
 	/*flash the LEDs RED and BLUE*/
-	set_led_strip_flash_state(RED,BLUE,500);
+	//set_led_strip_flash_state(RED,BLUE,500);
 	
 	/*wait for test button*/
-	wait_for_test_button();
+	//wait_for_test_button();
 	
 	/*flash the LEDs GREEN and YELLOW faster*/
-	set_led_strip_flash_state(GREEN,YELLOW,150);
+	//set_led_strip_flash_state(GREEN,YELLOW,150);
 	
 	/*wait for coin acceptor*/
-	wait_for_coin_insertion();
+	//wait_for_coin_insertion();
 	
 	/*turn off led flashing*/
-	reset_led_strip_flash_state();
+	//reset_led_strip_flash_state();
 	
 	/*turn off the LED strip*/
-	set_led_strip_color(OFF);
+	//set_led_strip_color(OFF);
 
 	/*and open the door 5 times*/
-	for(i=0;i<5;i++){
+	/*for(i=0;i<5;i++){
 		open_door();
 		delay(2000);
 	}
-	
+*/
 	exit(0);
 }
--- src/mindbx_testbench/src/mindbx_lib.c
+++ src/mindbx_testbench/src/mindbx_lib.c
@@ -1,35 +1,30 @@
 /**
- * @file mindbx_lib.c
- * @author Frederic Simard (frederic.simard.1@outlook.com) | atlants embedded
- * @brief Libraries of functions to interact with the mindbx hardware.
- *        Functions allow for the monitoring of the coin acceptor and test button, 
- *        and the control over LED strip and the solenoid that push the door open.
- */
-
+ * @file cerebral_app_lib.c
+ * @author Frederic Simard, Billy Labelle atlants embedded
+ * @brief */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <wiringPi.h>
+#include "wiringPi.h"
 
-#include "mindbx_lib.h"
 #include "pthread.h"
 
-#define	LED_STRIP_RED	0
-#define	LED_STRIP_GREEN	1
-#define	LED_STRIP_BLUE	2
-#define	SOLENOID		3
-#define	COIN_ACCEPTOR	4
-#define	TEST_BUTTON		5
+#define	LED_RED_P1  0
+#define	LED_GREEN,P1  1
+#define	LED_RED_P2	2
+#define	LED_GREEN_P2  		3
+#define	TEST_BUTTON		4
 
-#define FIRST_UNUSED_GPIO 6
+#define FIRST_UNUSED_GPIO 5
 #define TOT_NB_GPIO 8
 
-static char ls_flashing_latch = 0x00; 
+
+static char ls_flashing_latch = 0x00;
 static int ls_flashing_half_period = 0;
 static char ls_flashing_color_on = 0x00;
-static char ls_flashing_color_off = 0x00; 
+static char ls_flashing_color_off = 0x00;
 
-void* led_strip_flash(void* param);
+void *led_strip_flash(void *param);
 
 /**
  * setup_mindbx(void)
@@ -37,128 +32,156 @@ void* led_strip_flash(void* param);
  */
 void setup_mindbx(void)
 {
-	  int i ;
+	int i;
 
-	  /*check if root*/
-	  if (geteuid () != 0)
-	  {
-			fprintf (stderr, "mindbx need to be root to run\n") ;
-			exit (0) ;
-	  }
+	/*check if root */
+	if (geteuid() != 0) {
+		fprintf(stderr, "mindbx need to be root to run\n");
+		exit(0);
+	}
 
-	  /*setup the wiring pi*/
-	  if (wiringPiSetup () == -1)
-			exit (1) ;
+	/*setup the wiring pi */
+	if (wiringPiSetup() == -1)
+		exit(1);
 
-	  /*inform user*/
-	  printf("Wiringpi setup proceeding\n"); 
-	  fflush(stdout);
-	  
-	  /*define the pins functions*/
-	  pinMode(LED_STRIP_RED, OUTPUT);
-	  pinMode(LED_STRIP_GREEN, OUTPUT);
-	  pinMode(LED_STRIP_BLUE, OUTPUT);
-	  pinMode(SOLENOID, OUTPUT);
-	  pinMode(TEST_BUTTON, INPUT);
-	  pinMode(COIN_ACCEPTOR, INPUT);
-	  
-	  /*set default output values*/
-	  digitalWrite(SOLENOID, LOW);
-	  set_led_strip_color(OFF);
+	/*inform user */
+	printf("Wiringpi setup proceeding\n");
+	fflush(stdout);
 
-	  /*set other pins as output*/
-	  for (i=FIRST_UNUSED_GPIO ; i<TOT_NB_GPIO ; i++)
-	  {
-			pinMode(i, OUTPUT);
-			digitalWrite(i, LOW);
-	  }
+	/*define the pins functions */
+	pinMode(LED_RED_P1, OUTPUT);
+	pinMode(LED_GREEN_P1, OUTPUT);
+	pinMode(LED_RED_P2, OUTPUT);
+	pinMode(LED_GREEN_P2, OUTPUT);
+	pinMode(TEST_BUTTON, INPUT);
+	
 
-	  /*inform user*/
-	  printf ("Wiringpi setup completed\n");
+	/*set default output values */
+	set_led_strip_color(OFF);
+
+	/*set other pins as output */
+	for (i = FIRST_UNUSED_GPIO; i < TOT_NB_GPIO; i++) {
+		pinMode(i, OUTPUT);
+		digitalWrite(i, LOW);
+	}
+
+	/*inform user */
+	printf("Wiringpi setup completed\n");
 }
-
+/*
 /**
  * wait_for_coin_insertion(void)
  * @brief blocking call that waits for coin insertion
  */
-void wait_for_coin_insertion(void)
+/*void wait_for_coin_insertion(void)
 {
-	  printf("Waiting for a coin... "); 
-	  fflush(stdout);
-	  
-	  /*wait for coin acceptor to be activated*/
-	  while(digitalRead(COIN_ACCEPTOR)==HIGH && digitalRead(TEST_BUTTON)==HIGH)
-		delay(50);
-	  
-	  /*inform user*/
-	  printf("Coin accepted!\n");
-}
+	printf("Waiting for a coin... ");
+	fflush(stdout);
 
+	/*wait for coin acceptor to be activated */
+	/*while (digitalRead(COIN_ACCEPTOR) == HIGH && digitalRead(TEST_BUTTON) == HIGH)
+		delay(50);
+
+	/*inform user */
+	/*printf("Coin accepted!\n");
+}
+*/
 /**
  * wait_for_test_button(void)
  * @brief blocking call that waits for the test button
  */
 void wait_for_test_button(void)
 {
-	  printf("Waiting for a test button... "); 
-	  fflush(stdout);
-	  
-	  /*wait for test button to be activated*/
-	  while (digitalRead(TEST_BUTTON)==HIGH)
-		delay(50);
-	  
-	  /*inform user*/
-	  printf("Test button pressed!\n");
+	printf("Waiting for a test button... ");
+	fflush(stdout);
+
+	/*wait for test button to be activated */
+	while (digitalRead(TEST_BUTTON) == HIGH)
+		delay(100);
+
+	/*inform user */
+	printf("Test button pressed!\n");
 }
 
-/**
- * set_led_strip_color(void)
- * @brief sets the value of the PWM driving the led strip
- * @param intensity, {0,1024}
- */
-void set_led_strip_color(char rgb)
+void set_player1_green()
 {
-  digitalWrite(LED_STRIP_RED, rgb & RED ? HIGH : LOW);
-  digitalWrite(LED_STRIP_GREEN, rgb & GREEN ? HIGH : LOW);
-  digitalWrite(LED_STRIP_BLUE, rgb & BLUE ? HIGH : LOW);
+	digitalWrite(LED_GREEN_P1, 1);
+	
+}
+void reset_player1_green()
+{
+	digitalWrite(LED_GREEN_P1,0);
+	
+}
+void set_player2_green()
+{
+	digitalWrite(LED_GREEN_P2,1);
+	
+}
+void reset_player2_green()
+{
+	digitalWrite(LED_GREEN_P2,0);
+	
+}
+void set_player1_red()
+{
+	digitalWrite(LED_RED_P1,1);
+	
+}
+void reset_player1_red()
+{
+	digitalWrite(LED_RED_P1,0);
+	
+}
+void set_player2_red()
+{
+	digitalWrite(LED_RED_P2,1);
+	
+}
+void reset_player2_red()
+{
+	digitalWrite(LED_RED_P2,0);
+	
 }
 
 /**
  * open_door(void)
  * @brief push open the door of the mindbx. Solenoid stays activated for 5 seconds
  */
-void open_door(void)
+ 
+/*void open_door(void)
 {
 
-  /*open the door*/
-  digitalWrite(SOLENOID, HIGH);
-  printf ("Door opened!\n");
-  delay(5000);
-  digitalWrite(SOLENOID, LOW);
-  printf ("Can close door now\n");
+	/*open the door */
+	/*digitalWrite(SOLENOID, HIGH);
+	printf("Door opened!\n");
+	delay(5000);
+	digitalWrite(SOLENOID, LOW);
+	printf("Can close door now\n");
 
 }
-
+*/
 /**
  * set_led_strip_flash_state(void)
  * @brief 
  * @param 
  */
-void set_led_strip_flash_state(char rgb_on, char rgb_off, int half_period){
+void set_led_strip_flash_state(char rgb_on, char rgb_off, int half_period)
+{
 
 	pthread_t thread;
 	pthread_attr_t attr;
-	
+
 	ls_flashing_half_period = half_period;
 	ls_flashing_color_on = rgb_on;
 	ls_flashing_color_off = rgb_off;
-	
-	if(!ls_flashing_latch){
+
+	if (!ls_flashing_latch) {
 		ls_flashing_latch = 0x01;
-		
+
 		pthread_attr_init(&attr);
 		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-		pthread_create(&thread, &attr, led_strip_flash, (void*) NULL); 
+		pthread_create(&thread, &attr, led_strip_flash, (void *)NULL);
 		pthread_attr_destroy(&attr);
 	}
 }
@@ -168,17 +191,18 @@ void set_led_strip_flash_state(char rgb_on, char rgb_off, int half_period){
  * @brief 
  * @param 
  */
-void* led_strip_flash(void* param){
+void *led_strip_flash(void *param)
+{
 
-	printf ("Starts LED flashing\n");
-  
-	while(ls_flashing_latch){ 
-		set_led_strip_color(ls_flashing_color_on); 
+	printf("Starts LED flashing\n");
+
+	while (ls_flashing_latch) {
+		set_led_strip_color(ls_flashing_color_on);
 		delay(ls_flashing_half_period);
-		set_led_strip_color(ls_flashing_color_off); 
+		set_led_strip_color(ls_flashing_color_off);
 		delay(ls_flashing_half_period);
 	}
-	
+
 	return NULL;
 }
 
@@ -186,7 +210,57 @@ void* led_strip_flash(void* param){
  * reset_led_strip_flash_state(void)
  * @brief 
  */
-void reset_led_strip_flash_state(void){
-	printf ("Stops LED flashing\n");
+void reset_led_strip_flash_state(void)
+{
+	printf("Stops LED flashing\n");
 	ls_flashing_latch = 0x00;
 }
+
+/***********************************************************************/
+void set_player1_flash_red(){
+	
+	while(ls_flashing_latch){
+		digitalWrite(LED_RED_P1,HIGH);
+		delay(ls_flashing_half_period);
+		digitalWrite(LED_RED_P1,LOW);
+		delay(ls_flashing_half_period);		
+		}
+	
+	}
+/***********************************************************************/
+/***********************************************************************/
+void set_player1_flash_green(){
+	
+	while(ls_flashing_latch){
+		digitalWrite(LED_GREEN_P1,HIGH);
+		delay(ls_flashing_half_period);
+		digitalWrite(LED_GREEN_P1,LOW);
+		delay(ls_flashing_half_period);		
+		}
+	
+	}
+/***********************************************************************/
+/***********************************************************************/
+void set_player2_flash_red(){
+	
+	while(ls_flashing_latch){
+		digitalWrite(LED_RED_P2,HIGH);
+		delay(ls_flashing_half_period);
+		digitalWrite(LED_RED_P2,LOW);
+		delay(ls_flashing_half_period);		
+		}
+	
+	}
+/***********************************************************************/
+/***********************************************************************/
+void set_player2_flash_green(){
+	
+	while(ls_flashing_latch){
+		digitalWrite(LED_GREEN_P1,HIGH);
+		delay(ls_flashing_half_period);
+		digitalWrite(LED_GREEN_P1,LOW);
+		delay(ls_flashing_half_period);		
+		}
+	
+	}
+/***********************************************************************/
