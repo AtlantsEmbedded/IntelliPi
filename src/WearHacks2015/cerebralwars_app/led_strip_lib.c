4073ecd297e2432078e4fdca3e554d9bf9c2cdf7
atom2626 <atom2626@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-10-04 16:00:50 +0000

    Fix led_strip

--- src/WearHacks2015/cerebralwars_app/led_strip_lib.c
+++ src/WearHacks2015/cerebralwars_app/led_strip_lib.c
@@ -50,42 +50,8 @@ static unsigned char particle_color[2] = {0x00,0x00};
 unsigned char led_strip_alive = 0x00;
 pthread_t led_strip_thread;
 
-
 void* led_strip_task(void* param);
 
-/**
- * main(int argc, char **argv)
- * @brief test the mindbx_lib
- */
-int main(int argc, char **argv){
-	
-	int i;
-	
-	printf("Getting started\n");
-	fflush(stdout); 
-	
-	init_led_strip();
-	
-	printf("Waiting 20seconds\n");
-	fflush(stdout); 
-	
-	sleep(20);
-	
-	printf("Sliding the explosion\n");
-	fflush(stdout); 
-	
-	for(i=0;i<100;i++){
-		set_led_strip_values(1, 1, 10+i);
-		sleep(1);
-	}
-	
-	
-	printf("Done! Cleaning up\n");
-	fflush(stdout); 
-	cleanup_led_strip();
-	
-	exit(0);
-}
 
 
 void init_led_strip(){
