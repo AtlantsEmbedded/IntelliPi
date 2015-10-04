22ac3cdb47385ba70eefa9e0a8a0c61a2d5b61de
atom2626 <atom2626@8e7d357a-65eb-4e78-97b1-12d5d4625717>
2015-10-04 16:43:59 +0000

    Fix for led

--- src/WearHacks2015/cerebralwars_app/include/led_strip_lib.h
+++ src/WearHacks2015/cerebralwars_app/include/led_strip_lib.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NB_LEDS 149   
+
 void init_led_strip();
 void set_led_strip_values(int red_player_period, int blue_player_period, int contact_location);
 void cleanup_led_strip();
