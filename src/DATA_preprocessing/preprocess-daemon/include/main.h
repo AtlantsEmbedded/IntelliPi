ffd30319132f3ab0721591487fb251467e1a31bf
fred simard <atom2626@hotmail.com>
2016-01-21 16:04:55 -0500

    Removing code duplicat

--- src/DATA_preprocessing/preprocess-daemon/include/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*clean up function, called on signal*/
-void cleanup_app();
-
-#endif
--- src/braintone_app/include/buzzer_lib.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUZZER_LIB_H
-#define BUZZER_LIB_H
-
-int setup_buzzer_lib(void);
-int set_buzzer_state(int state);
-int set_beep_mode(unsigned char state_a, unsigned char state_b, int half_period);
-void turn_off_beeper();
-
-#endif
